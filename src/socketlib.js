manuelVo/foundryvtt-socketlib

commit 1646bbbe3bfea2b022f17598753a60704ce549e4

Don't send a package id for commands as it's not needed

src/socketlib.js
@@ -168,7 +168,6 @@ class SocketlibSocket {
 
 	_sendCommand(handlerName, args, recipient) {
 		const message = {handlerName, args, recipient};
-		message.id = randomID();
 		message.type = MESSAGE_TYPES.COMMAND;
 		game.socket.emit(this.socketName, message);
 	}
@@ -241,7 +240,7 @@ class SocketlibSocket {
 			[name, func] = this._resolveFunction(handlerName);
 		}
 		catch (e) {
-			if (e instanceof errors.SocketlibUnregisteredHandlerError) {
+			if (e instanceof errors.SocketlibUnregisteredHandlerError && type === MESSAGE_TYPES.REQUEST) {
 				this._sendError(id, MESSAGE_TYPES.UNREGISTERED);
 			}
 			throw e;
@@ -258,7 +257,6 @@ class SocketlibSocket {
 			}
 			catch (e) {
 				console.error(`An exception occured while executing handler '${name}'.`);
-				this._sendError(id, MESSAGE_TYPES.EXCEPTION);
 				throw e;
 			}
 			this._sendResult(id, result);

commit fb3aeaf8139491dda17e80113dfb795c59172ee3

Fix a regression introduced in 1646bbbe that prevented remote errors to propagate to the calling client

src/socketlib.js
@@ -258,6 +258,7 @@ class SocketlibSocket {
 			}
 			catch (e) {
 				console.error(`An exception occured while executing handler '${name}'.`);
+				this._sendError(id, MESSAGE_TYPES.EXCEPTION);
 				throw e;
 			}
 			this._sendResult(id, result);